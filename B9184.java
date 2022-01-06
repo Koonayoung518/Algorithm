5911338d3f0465faffeb3e65c6d6d637f7f2f7ab
Koonayoung518 <kny415@naver.com>
2022-01-06 15:20:10 +0900

    신나는 함수 실행/재귀함수 dp[][][]

---
+++ B9184.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class B9184 {
+    static int[][][] dp = new int[21][21][21];
+    static int w(int a, int b, int c){
+        if(a<=0 || b<=0 || c<=0) return 1;
+        if(a>20 || b>20 || c>20) return dp[20][20][20] =w(20,20,20);
+        if(dp[a][b][c] !=0) return dp[a][b][c];  
+        if(a<b && b<c)
+            return dp[a][b][c] = w(a,b,c-1) + w(a,b-1,c-1) - w(a,b-1,c);
+        return dp[a][b][c] = w(a-1,b,c) + w(a-1,b-1,c) + w(a-1,b,c-1)- w(a-1,b-1,c-1);
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        while(true) {
+            String[] input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+            int c = Integer.parseInt(input[2]);
+            if(a == -1 && a == b && a == c) break;
+            bw.write("w(" + a + ", " + b + ", " + c + ") = " + w(a,b,c) + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
