64cc0e00355cf43e2c6f13ae8fb973864851ca62
Koonayoung518 <kny415@naver.com>
2022-02-20 20:23:40 +0900

    A/B

---
+++ B1008.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class B1008 {
+	
+	public static void main(String[] args) {
+		double a,b;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		a = sc.nextDouble();
+		b = sc. nextDouble();
+		System.out.print(a/b);
+	}
+	}
